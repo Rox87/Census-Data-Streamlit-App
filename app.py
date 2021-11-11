cf3975f7c33f58b237985e66f7154a85d283fad5
Rox87 <rodrigohbx@gmail.com>
2021-11-11 16:42:43 -0300

    app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+st.title('Census Data Exploration')
+st.header('Explore socially important metrics')
+st.write('This exploration uses data from American Community Survey 5-year data released in 2017.')
+st.write('More information about the ACS 5-year survey: https://www.census.gov/data/developers/data-sets/acs-5year.html')
+st.write("The data was collected using Julien Leider's CensusData pip package, which can be found at https://pypi.org/project/CensusData/ or installed with")
+
+
+st.header('County Level Summaries')
+st.write('This analysis will use data aggregated on the county level for the variables: Gini Index (income inequality index), Vacant Housing, Percent Unemployed and Median Family Income.')
+
+
+
+
+
+
+
